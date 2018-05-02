010474e02cfdf8b534e0aaf69eac317cb2733753
Rui-Santos <rui.s@gmx.pt>
2018-05-02 11:35:48 +0100

    Check if PPA exists beforing adding it

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 WITH_DOCKER=false
+PPA=valhalla-core/valhalla
 for arg in "$@"; do
   shift
   case "$arg" in
@@ -14,8 +15,11 @@ then
     docker build -t tpportugal/tpp_valhalla:latest .
 else
     sudo apt-get install -y software-properties-common
-    sudo add-apt-repository -y ppa:valhalla-core/valhalla
+    if ! grep -q "^deb .*$PPA" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+      sudo add-apt-repository -y ppa:valhalla-core/valhalla
+    fi
     sudo apt-get update
+    sudo apt-get upgrade -y
     sudo apt-get install -y autoconf automake make libtool pkg-config g++ gcc jq lcov protobuf-compiler vim-common libboost-all-dev libboost-all-dev libcurl4-openssl-dev zlib1g-dev liblz4-dev libprime-server0.6.3-dev libprotobuf-dev prime-server0.6.3-bin
     sudo apt-get install -y libgeos-dev libgeos++-dev liblua5.2-dev libspatialite-dev spatialite-bin libsqlite3-dev lua5.2 libsqlite3-mod-spatialite
     sudo apt-get install -y python-all-dev
