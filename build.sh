349c249ebd150c23d8a6d095ce7ddc39cba98124
Rui-Santos <rui.s@gmx.pt>
2018-05-02 09:46:09 +0100

    apt-get and make install need sudo if without docker

--- build.sh
+++ build.sh
@@ -7,19 +7,21 @@ for arg in "$@"; do
   esac
 done
 
-if [ $WITH_DOCKER = true ] 
+git submodule update --init --recursive
+
+if [ $WITH_DOCKER = true ]
 then
     docker build -t tpportugal/tpp_valhalla:latest .
 else
-    apt-get install -y software-properties-common
-    add-apt-repository -y ppa:valhalla-core/valhalla
-    apt-get update
-    apt-get install -y autoconf automake make libtool pkg-config g++ gcc jq lcov protobuf-compiler vim-common libboost-all-dev libboost-all-dev libcurl4-openssl-dev zlib1g-dev liblz4-dev libprime-server0.6.3-dev libprotobuf-dev prime-server0.6.3-bin
-    apt-get install -y libgeos-dev libgeos++-dev liblua5.2-dev libspatialite-dev spatialite-bin libsqlite3-dev lua5.2
-    apt-get install -y python-all-dev
+    sudo apt-get install -y software-properties-common
+    sudo add-apt-repository -y ppa:valhalla-core/valhalla
+    sudo apt-get update
+    sudo apt-get install -y autoconf automake make libtool pkg-config g++ gcc jq lcov protobuf-compiler vim-common libboost-all-dev libboost-all-dev libcurl4-openssl-dev zlib1g-dev liblz4-dev libprime-server0.6.3-dev libprotobuf-dev prime-server0.6.3-bin
+    sudo apt-get install -y libgeos-dev libgeos++-dev liblua5.2-dev libspatialite-dev spatialite-bin libsqlite3-dev lua5.2 libsqlite3-mod-spatialite
+    sudo apt-get install -y python-all-dev
     ./autogen.sh
     ./configure
     make test -j$(nproc)
-    make install
+    sudo make install
     ldconfig
 fi
